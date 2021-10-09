59aeca998ff2ccab9a732b66b7366eb8cbec1af1
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-10-09 19:28:53 +0330

    hidden commit msg

--- compiler.py
+++ compiler.py
@@ -1,5 +1,28 @@
-def get_next_token():
+# def get_next_token():
+#     pass
+
+class ErrorHandler:
     pass
+
+
+class SymbolTableHandler:
+    pass
+
+
+class State:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def get_next_state(self, character: str):
+        raise NotImplementedError
+
+
+class LexicalAnalyzer:
+    def __init__(self) -> None:
+        self.state = State('init')
+        self.token = ''
+        self.index = 0
+        self.line_number = 1
 
 
 if __name__ == '__main__':
