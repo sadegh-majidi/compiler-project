daf83b8d6960fe8a033ea2644bc770ae47398f57
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-10-16 01:16:14 +0330

    majima

--- compiler.py
+++ compiler.py
@@ -1,4 +1,6 @@
-from state import STATES
+import re
+
+from state import STATES, REGEX
 
 
 class ErrorHandler:
@@ -6,7 +8,25 @@ class ErrorHandler:
 
 
 class SymbolTableHandler:
-    pass
+    table = dict()
+
+    @staticmethod
+    def init_table():
+        for keyword in ['if', 'else', 'void', 'int', 'repeat', 'break', 'until', 'return']:
+            with open('symbol_table.txt', 'a') as f:
+                SymbolTableHandler.table[keyword] = {'type': 'keyword'}
+                f.write(f'{len(SymbolTableHandler.table)}.\t{keyword}\n')
+
+    @staticmethod
+    def add_token_to_table(token: tuple):
+        if token[1] not in SymbolTableHandler.table:
+            with open('symbol_table.txt', 'a') as f:
+                SymbolTableHandler.table[token[1]] = {'type': token[0]}
+                f.write(f'{len(SymbolTableHandler.table)}.\t{token[1]}\n')
+
+    @staticmethod
+    def is_token_keyword(token: str):
+        return bool(re.match(REGEX['keyword'], token))
 
 
 class LexicalAnalyzer:
@@ -18,6 +38,7 @@ class LexicalAnalyzer:
         self.valid_tokens = []
         with open('input.txt', 'r') as f:
             self.input = f.read()
+        SymbolTableHandler.init_table()
 
     def get_next_token(self):
         index = self.index
@@ -37,7 +58,14 @@ class LexicalAnalyzer:
                 index -= 1
 
                 if self.state.name in {'number', 'identifier', 'symbol'}:
-                    self.valid_tokens.append((self.state.name, self.token))
+                    token_type = self.state.name
+                    if token_type == 'identifier' and SymbolTableHandler.is_token_keyword(self.token):
+                        token_type = 'keyword'
+
+                    token = (token_type, self.token)
+                    self.valid_tokens.append(token)
+                    if token_type == 'identifier':
+                        SymbolTableHandler.add_token_to_table(token)
                     self.index = index
                     self.state = STATES['initial']
                     self.token = ''
@@ -48,7 +76,27 @@ class LexicalAnalyzer:
                 continue
 
             if char == '\n':
+                self.flush_tokens()
                 self.line_number += 1
+
+    def flush_tokens(self):
+        if not self.valid_tokens:
+            return
+
+        token_type_translator = {
+            'number': 'NUM',
+            'identifier': 'ID',
+            'symbol': 'SYMBOL',
+            'keyword': 'KEYWORD'
+        }
+
+        with open('tokens.txt', 'a') as f:
+            f.write(f'{self.line_number}.\t')
+            for token in self.valid_tokens:
+                f.write(f'({token_type_translator[token[0]]}, {token[1]}) ')
+            f.write('\n')
+
+        self.valid_tokens.clear()
 
 
 if __name__ == '__main__':
