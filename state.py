f6fe0ea1065e2aec3754f993b031fb0bd3506aa3
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-10-20 02:27:51 +0330

    hidden commit msg

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 import re
 
-from errors import InvalidNumberError, UnmatchedCommentError
+from errors import InvalidNumberError, UnmatchedCommentError, InvalidInputError
 
 
 class State:
@@ -18,7 +18,7 @@ REGEX = {
     'digit': r'^\d$',
     'alphabet': r'^[a-zA-Z]$',
     'keyword': r'^if|else|void|int|repeat|break|until|return$',
-    'symbol': r'^;|:|\[|]|\(|\)|{|}|\+|-|\*|=|<$',
+    'symbol': r'^;|:|\[|]|\(|\)|{|}|\+|-|\*|=|<|,$',
     'whitespace': r'^\n|\r|\t|\v|\f| $',
     'new_line': r'^\n$',
     'slash': r'^/$',
@@ -43,11 +43,19 @@ class InitialState(State):
                 state.star_detected = True
             else:
                 state.star_detected = False
+
+            if re.match(REGEX['equal_sign'], character):
+                state.double_equal = True
+            else:
+                state.double_equal = False
+
             return state
         if re.match(REGEX['whitespace'], character):
             return STATES['whitespace']
         if re.match(REGEX['slash'], character):
             return STATES['comment']
+
+        raise InvalidInputError
 
 
 class NumberState(State):
@@ -62,7 +70,9 @@ class NumberState(State):
         if re.match(REGEX['slash'], character):
             return STATES['comment']
         if re.match(REGEX['alphabet'], character):
-            raise InvalidNumberError()
+            raise InvalidNumberError
+
+        raise InvalidInputError
 
 
 class IdentifierState(State):
@@ -77,6 +87,8 @@ class IdentifierState(State):
         if re.match(REGEX['slash'], character):
             return STATES['comment']
 
+        raise InvalidInputError
+
 
 class SymbolState(State):
     double_equal = False
@@ -84,32 +96,40 @@ class SymbolState(State):
 
     def get_next_state(self, character: str):
         if re.match(REGEX['slash'], character):
+            self.double_equal = False
             if self.star_detected:
                 self.star_detected = False
                 raise UnmatchedCommentError
             self.star_detected = False
             return STATES['comment']
         if re.match(REGEX['digit'], character):
+            self.double_equal = False
             self.star_detected = False
             return STATES['number']
         if re.match(REGEX['alphabet'], character):
+            self.double_equal = False
             self.star_detected = False
             return STATES['identifier']
         if re.match(REGEX['equal_sign'], character):
             self.star_detected = False
             if self.double_equal:
                 self.double_equal = False
-                return STATES['initial']
-            else:
-                self.double_equal = True
                 return STATES['symbol']
+            else:
+                self.double_equal = False
+                return STATES['initial']
         if re.match(REGEX['symbol'], character):
             self.star_detected = False
+            self.double_equal = False
             return STATES['initial']
         if re.match(REGEX['whitespace'], character):
             self.star_detected = False
+            self.double_equal = False
             return STATES['whitespace']
         self.star_detected = False
+        self.double_equal = False
+
+        raise InvalidInputError
 
 
 class CommentState(State):
@@ -119,6 +139,8 @@ class CommentState(State):
             return STATES['multi_line_comment']
         if re.match(REGEX['slash'], character):
             return STATES['single_line_comment']
+
+        raise InvalidInputError
 
 
 class SingleLineCommentState(State):
@@ -135,18 +157,22 @@ class MultiLineCommentState(State):
     comment_ended = False
 
     def get_next_state(self, character: str):
-        if re.match(REGEX['slash'], character):
+        if re.match(REGEX['slash'], character) and (not self.comment_ended):
             if self.star_detected:
                 self.star_detected = False
                 self.comment_ended = True
                 return STATES['multi_line_comment']
             else:
+                self.comment_ended = False
+                self.star_detected = False
                 return STATES['multi_line_comment']
 
         if self.comment_ended:
             self.comment_ended = False
+            self.star_detected = False
             return STATES['initial']
 
+        self.comment_ended = False
         if re.match(REGEX['star'], character):
             self.star_detected = True
             return STATES['multi_line_comment']
@@ -168,6 +194,8 @@ class WhitespaceState(State):
             return STATES['whitespace']
         if re.match(REGEX['slash'], character):
             return STATES['comment']
+
+        return STATES['initial']
 
 
 STATES = {
